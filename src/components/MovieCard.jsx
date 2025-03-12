232250c31e8b4e5fb4bd486cc0b8b8ebc4119e91
code4daniel <code4daniel@gmail.com>
2025-03-12 21:01:07 +0300

    Creating neccessary files

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MovieCard = () => {
+  return (
+    <div>
+      cards
+    </div>
+  )
+}
+
+export default MovieCard
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter,createRoutesFromElements, RouterProvider,Route } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// componentts
+import App from './App.jsx'
+import Home from './pages/Home.jsx'
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    
+    <Route path='/' element={<App/>}>
+      <Route path='/' element={<Home/>}/>
+      
+    </Route>
+  )
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
 )
